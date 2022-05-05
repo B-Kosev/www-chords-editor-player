e779bb29112ccba7105f407dd5e22380bd40ddd8
Богдан Косев <bogdan_gamer@abv.bg>
2022-05-06 00:27:23 +0300

    add chord model, add classes to piano keys

--- php/model/Chord.php
+++ php/model/Chord.php
@@ -1,8 +1,46 @@
 <?php
 
 class Chord {
-    private $tonic;
-    private $asd;
-    private $dominant;
+    private $id; // 'c' or 'cm' etc
 
+    private $name; // C Minor
+
+    private $first; // 1c
+    private $third; // 1ds
+    private $fifth; // 1g
+
+    private $inversion;
+
+    public function __construct(string $id, string $name, string $first, string $third, string $fifth, int $inversion){
+        $this->id = $id;
+        $this->name = $name;
+        $this->first = $first;
+        $this->third = $third;
+        $this->fifth = $fifth;
+        $this->inversion = $inversion;
+    }
+
+    public function getId(): string {
+        return $this->id;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getFirst(): string {
+        return $this->first;
+    }
+
+    public function getThird(): string {
+        return $this->third;
+    }
+
+    public function getFifth(): string {
+        return $this->fifth;
+    }
+
+    public function getInversion(): int {
+        return $this->inversion;
+    }
 }
